378ce840fc1d7b203f53acd63dd30c21bfc2afd4
Chase McBee <chasamcbee@tutamail.com>
2026-09-09 13:55:17 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Your Full Name
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 8, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Chase McBee.");
+Console.WriteLine("This is my first program at ETSU.");
